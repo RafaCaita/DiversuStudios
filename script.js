034b887db72aeb25a5b44b5c8c9af030c27278b9
RafaCaita <137647897+RafaCaita@users.noreply.github.com>
2026-01-21 15:40:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Menu Responsivo
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+navSlide();
